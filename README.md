5be6835f7e1a31d4ff0071249cbcdfd3ecde0bb3
samAd0san <sohaibsamad.hala@gmail.com>
2024-05-09 10:12:06 +0530

    Nav bar (sm)

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 # React Icon
 ```npm install react-icons --save```
+
+# CSS tailwind
+- <b>fixed</b> positions the element relative to the viewport, ensuring it stays in the same position even when the user scrolls.
+- <b>top-0, right-0, and left-0</b> set the element's distance from the top, right, and left edges of the viewport respectively, making the element span the entire width of the viewport at the top.
+
+
+
+
+
--- src/compnents/NavBar.jsx
+++ src/compnents/NavBar.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import logo from '../assets/logo.png'
 // React Icons
 import { SlHandbag } from "react-icons/sl";
+import { FaBars, FaXmark } from "react-icons/fa6";
 
 function NavBar() {
-    
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     const NavItems = [
         { link: 'Overview', path: 'home' },
         { link: 'About', path: 'about' },
@@ -11,36 +19,59 @@ function NavBar() {
         { link: 'Activities', path: 'activities' }
     ];
 
-    return (<nav className='bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary'>
-        <div className='text-lg container mx-auto flex justify-between items-center font-medium'>
-            {/* Left Side of the Nav bar */}
-            <div className='flex items-center space-x-14 '>
-                {/* Logo + Icon */}
-                <a href="/" className='flex text-3xl items-center font-semibold space-x-3 text-primary'>
-                    <img className='w-11 h-11 inline-block items-center' src={logo} alt="Logo" /> <span>Logo</span>
-                </a>
+    return (
+        <>
+            {/* Nav Bar Primary */}
+            <nav className='bg-white md:px-14 p-4 max-w-screen-2xl mx-auto font-semibold text-primary fixed top-0 right-0 left-0'>
+                <div className='text-lg container mx-auto flex justify-between items-center font-medium'>
+                    {/* Left Side of the Nav bar */}
+                    <div className='flex items-center space-x-14 '>
+                        {/* Logo + Icon */}
+                        <a href="/" className='flex text-3xl items-center font-semibold space-x-3 text-primary'>
+                            <img className='w-11 h-11 inline-block items-center' src={logo} alt="Logo" /> <span>Logo</span>
+                        </a>
 
-                {/* Nav Items */}
-                <ul className='md:flex space-x-12 items-center mt-1 hidden'>
-                    {
-                        // Displaying all the nav items by iterating using map function
-                        NavItems.map(({ link, path }) => <a className='block hover:text-gray-300' key={link} href={path} >{link}</a>)
-                    }
-                </ul>
-            </div>
+                        {/* Nav Items */}
+                        <ul className='md:flex space-x-12 items-center mt-1 hidden'>
+                            {
+                                // Displaying all the nav items by iterating using map function
+                                NavItems.map(({ link, path }) => <a className='block hover:text-gray-300' key={link} href={path} >{link}</a>)
+                            }
+                        </ul>
+                    </div>
 
-            {/* Right Side of the Nav bar */}
-            <div className='md:flex space-x-12 items-center hidden'>
-                {/* Products (React Icon) + Sign up button */}
-                <a className='md:flex items-center hidden' href="/">
-                    <SlHandbag className='mr-2' /> <span className='hover:text-secondary mt-1'>Products</span>
-                </a>
-                <button className='bg-secondary py-2 px-3 rounded-lg hover:text-white hover:bg-cyan-400 transition-all duration-300'>
-                    Sign up
-                </button>
+                    {/* Right Side of the Nav bar */}
+                    <div className='md:flex space-x-12 items-center hidden'>
+                        {/* Products (React Icon) + Sign up button */}
+                        <a className='md:flex items-center hidden' href="/">
+                            <SlHandbag className='mr-2' /> <span className='hover:text-cyan-400 mt-1'>Products</span>
+                        </a>
+                        <button className='bg-secondary py-2 px-3 rounded-lg hover:text-white hover:bg-cyan-400 transition-all duration-300'>
+                            Sign up
+                        </button>
+                    </div>
+
+                    {/* Menu button on mobile device (Nav bar)*/}
+                    <div className='md:hidden'>
+                        <button onClick={toggleMenu} className='text-white'>
+                            {
+                                isMenuOpen ? (<FaXmark className='w-6 h-6 text-primary' />) : (<FaBars className='w-6 h-6 text-primary' />)
+                            }
+                        </button>
+                    </div>
+                </div>
+            </nav>
+
+            {/* Nav Items for Mobile */}
+            <div className={`md:hidden space-y-4 px-4 pt-24 pb-5 bg-secondary text-xl ${isMenuOpen ? 'block fixed top-0 right-0 left-0' : 'hidden'} `}>
+                {
+                    NavItems.map(({ link, path }) => <a key={link} href={path} className='block hover:text-white text-primary font-medium'>
+                        {link}
+                    </a>)
+                }
             </div>
-        </div>
-    </nav>);
+        </>
+    );
 }
 
 export default NavBar;
